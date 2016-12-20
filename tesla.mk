a928054073d32912bcffea44d53212e7a2b59de2
dfuse06 <dfuse06@gmail.com>
2016-12-20 08:46:27 -0600

    Derp

--- tesla.mk
+++ tesla.mk
@@ -1,8 +1,8 @@
 $(call inherit-product, device/zte/draconis/full_draconis.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/tesla/config/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := ZTE ZMAX
-PRODUCT_NAME := cm_draconis
+PRODUCT_NAME := tesla_draconis
 PRODUCT_PACKAGES += OTAUpdates
