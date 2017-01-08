de8b96b3d523edc16722ebd95739993d95c8dbd1
dfuse06 <dfuse06@gmail.com>
2017-01-07 22:41:50 -0600

    Updated

--- tesla.mk
+++ tesla.mk
@@ -3,6 +3,9 @@ $(call inherit-product, device/zte/draconis/full_draconis.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/tesla/config/common_full_phone.mk)
 
-PRODUCT_RELEASE_NAME := ZTE ZMAX
 PRODUCT_NAME := tesla_draconis
-PRODUCT_PACKAGES += OTAUpdates
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE="draconis" \
+    PRODUCT_NAME="draconis"
+
